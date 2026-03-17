d0fa3fe5ca4af1a39acd7734e01a7dd8f632c865
Rafik H <rafik.hns@gmail.com>
2026-03-17 10:29:50 +0000

    File changes

--- src/components/game/GameBoard.jsx
+++ src/components/game/GameBoard.jsx
@@ -24,10 +24,12 @@ export default function GameBoard({ gameState, selectedTerritory, phase, current
   };
 
   return (
+    <div style={{ overflowX: 'auto', overflowY: 'hidden' }}>
     <div
-      className="relative rounded-xl overflow-hidden"
+      className="relative rounded-xl"
       style={{
-        minHeight: '500px',
+        width: '900px',
+        height: '560px',
         backgroundImage: `url('https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69b732e420481df67e8a6804/62c454dca_NemRma69YXUdPY6orqTHe3.jpg')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
